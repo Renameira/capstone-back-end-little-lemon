516c3021df2e52fe68b0424f4f3184eef92c0558
renameira <renatomeira91@gmail.com>
2023-10-09 22:00:32 -0300

    Add new function in models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -11,3 +11,6 @@ class MenuItens(models.Model):
     title = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2,)
     invetory = models.IntegerField
+
+    def __str__(self):
+        return f'{self.title} : {str(self.price)}'
